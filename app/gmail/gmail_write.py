ca94cacccd2bdce2c70ea58692876b94f97799ee
fadhkiko <mmfadhh@gmail.com>
2026-09-10 14:19:23 +0530

    Update gmail_write.py

--- app/gmail/gmail_write.py
+++ app/gmail/gmail_write.py
@@ -1,1 +1,12 @@
+import os
+import re
+import urllib.parse
 
+CLIENT_EMAIL = os.getnv("CLIENT_EMAIL","")
+
+KEYWORDS = {
+  "gmail", "email", "e_mail", "mail".
+  "write an email", "send an email","draft an email",
+"compose an email", "write mail", "send mail","draft mail",
+"compose mail"
+}
